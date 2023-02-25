459457f8ac8cf1ec6f1915f368e8b321006eea62
Tembulat Tokmakov <tembatokmakov43@gmail.com>
2023-02-25 16:33:13 +0300

    commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,7 +1,11 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public long sum(long[] sales) {
+        long result = 0;
+        for (long sale : sales) {
+            result += sale;
+        }
+        return result;
     }
 }
